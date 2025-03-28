73bb7fe58bf334da2165e946e943eb01bf027210
Guilherme Souza <152984208+GUi050723@users.noreply.github.com>
2025-03-28 20:51:41 -0300

    Add files via upload

---
+++ Raiodocirculo/raiocirculo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+float raio;
+cout << "Digite o valor do raio do circulo";
+cin >> raio;
+float area = 3.14*(raio*raio); 
+cout << "O valor da area do circulo e:"<< area << endl;
+}
