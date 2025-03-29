6382b8aa7443095567cc98b1ed96c62c50eac808
Guilherme Souza <152984208+GUi050723@users.noreply.github.com>
2025-03-28 21:03:32 -0300

    Add files via upload

---
+++ EXdiferencaABCD/diferencaABCD/Program.cs
@@ -0,0 +1,15 @@
+﻿int A,B,C,D,diferenca;
+System.Console.WriteLine("Digite 4 numeros inteiros");
+Console.Write("A:");
+A = int.Parse(Console.ReadLine());
+
+Console.Write("B:");
+B = int.Parse(Console.ReadLine());
+
+Console.Write("C:");
+C = int.Parse(Console.ReadLine());
+
+Console.Write("D:");
+D = int.Parse(Console.ReadLine());
+diferenca = (A*B)-(C*D);
+System.Console.WriteLine("O resultado e: "+diferenca);
